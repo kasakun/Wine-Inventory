d49bd047d506188752d91235f9e0299f1344cb88
Zeyu Chen <chenzy@gatech.edu>
2018-03-15 01:36:04 -0400

    Cart, History, Login added

--- wine.py
+++ wine.py
@@ -2,6 +2,7 @@
 # [START imports]
 import os
 import urllib
+import random
 
 from google.appengine.api import users
 from google.appengine.ext import ndb
@@ -17,29 +18,40 @@ JINJA_ENVIRONMENT = jinja2.Environment(
 # [END imports]
 
 DEFAULT_WINE_CAT = 'red'
-
-
-# We set a parent key on the 'Greetings' to ensure that they are all
-# in the same entity group. Queries across the single entity group
-# will be consistent. However, the write rate should be limited to
-# ~1/second.
+## charlist
+CHAR = [chr(i) for i in xrange(ord('A'), ord('Z')+1)] \
+                    + [chr(i) for i in xrange(ord('a'), ord('z')+1)] \
+                    + [chr(i) for i in xrange(ord('0'), ord('9')+1)]
 
 def wine_key(wine_cate=DEFAULT_WINE_CAT):
     """Constructs a Datastore key for a Wine entity."""
     return ndb.Key('WineCategory', wine_cate.lower())
-def cart_key(user):
+def user_key(user):
     return ndb.Key('User', user)
 
 class wine(ndb.Model):
     """A main model for representing an individual Wine entry."""
+    id = ndb.StringProperty(indexed = True)
     country = ndb.StringProperty(indexed = True)
     region = ndb.StringProperty(indexed = True)
     variety = ndb.StringProperty(indexed = True)
     name = ndb.StringProperty(indexed = True)
     year = ndb.StringProperty(indexed = True)
-    price = ndb.FloatProperty(indexed=False)
+    price = ndb.FloatProperty(indexed = True)
     date = ndb.DateTimeProperty(auto_now_add = True)
 
+
+class cart_entry(ndb.Model):
+    wine_id = ndb.StringProperty(indexed = True)
+    wine_num = ndb.IntegerProperty(indexed = True)
+    date = ndb.DateTimeProperty(auto_now_add = True)
+
+
+class history_entry(ndb.Model):
+    user_id = ndb.StringProperty(indexed = True)
+    wine_id = ndb.StringProperty(indexed = True)
+    wine_num = ndb.IntegerProperty(indexed = True)
+    date = ndb.DateTimeProperty(auto_now_add = True)
 
 # [START main_page]
 class MainPage(webapp2.RequestHandler):
@@ -60,15 +72,20 @@ class MainPage(webapp2.RequestHandler):
             url_linktext = 'Login'
 
         template_values = {
+            'user': user,
+            'cookie_id': cookie_id,
             'url': url,
             'url_linktext': url_linktext
         }
 
         template = JINJA_ENVIRONMENT.get_template('index.html')
         self.response.write(template.render(template_values))
+        self.response.headers.add_header('Set-Cookie', 'key=%s' % str(cookie_id))
+
+
 # [END main_page]
 
-# Wine
+# Wine Library
 class Wine(webapp2.RequestHandler):
     def get(self):
         cate = self.request.get('WineCategory').lower()
@@ -89,6 +106,7 @@ class Wine(webapp2.RequestHandler):
 
         wines = wine_query.fetch(50)
         wine_values = {
+            'user': user,
             'wines': wines,
             'cate': cate,
             'url': url,
@@ -117,6 +135,7 @@ class Enter(webapp2.RequestHandler):
             url = users.create_login_url('/')
             url_linktext = 'Login'
         template_values = {
+            'user': user,
             'url': url,
             'url_linktext': url_linktext
         }
@@ -125,13 +144,17 @@ class Enter(webapp2.RequestHandler):
 
     def post(self):
         cate = self.request.get('WineCategory', 'red')
-        print cate
         new_wine = wine(parent = wine_key(cate.lower()))
+        # generate wine unique id
+        new_wine.id = ''
+        for i in xrange(20):
+            new_wine.id += CHAR[random.randint(0, len(CHAR) - 1)]
         new_wine.country = self.request.get('country')
         new_wine.region = self.request.get('region')
         new_wine.variety = self.request.get('variety')
         new_wine.name = self.request.get('name')
         new_wine.year = self.request.get('year')
+        new_wine.price = float(self.request.get('price'))
         new_wine.put()
         query_params = {'WineCategory': cate.lower()}
         self.redirect('/wine?' + urllib.urlencode(query_params))
@@ -149,6 +172,7 @@ class Search(webapp2.RequestHandler):
             url = users.create_login_url('/')
             url_linktext = 'Login'
         template_values = {
+            'user': user,
             'url': url,
             'url_linktext': url_linktext
         }
@@ -279,11 +303,156 @@ class Search(webapp2.RequestHandler):
         template = JINJA_ENVIRONMENT.get_template('search.html')
         self.response.write(template.render(wine_values))
 
+# Cart
+class Cart(webapp2.RequestHandler):
+    def get(self):
+        user = users.get_current_user()
+        if user:
+            login_sign = 1
+            user = user.email()
+            url = users.create_logout_url('/')
+            url_linktext = 'Logout'
+            cookie_id = self.request.cookies.get('key')
+            # check if have temporary cart
+            cart_temp = cart_entry.query(ancestor = user_key(cookie_id))
+            if cart_temp:
+                for entry in cart_temp:
+                    new_entry = cart_entry(parent = user_key(user))
+                    new_entry.wine_id = entry.wine_id
+                    new_entry.wine_num = entry.wine_num
+                    new_entry.put()
+                    entry.key.delete()
+        else:
+            login_sign = 0
+            user = self.request.cookies.get('key')
+            url = users.create_login_url('/')
+            url_linktext = 'Login'
+
+        entry_query = cart_entry.query(ancestor = user_key(user)).order(-cart_entry.date)
+        entries = entry_query.fetch()
+
+        wines = []
+        total = 0.00
+        for entry in entries:
+            wine_query = wine.query()
+            wine_tmps = wine_query.filter(wine.id == entry.wine_id).fetch(1)
+            if  wine_tmps:
+                wine_tmp = wine_tmps.pop(0)
+                # wine_tmp = make_pairs(entry.wine_num)
+                wines.append((wine_tmp, entry.wine_num))
+                total = total + wine_tmp.price*entry.wine_num
+
+
+        template_values = {
+            'user': user,
+            'wines': wines,
+            'total': total,
+            'login_sign': login_sign,
+            'url': url,
+            'url_linktext': url_linktext
+        }
+
+        template = JINJA_ENVIRONMENT.get_template('cart.html')
+        self.response.write(template.render(template_values))
+
+## update items in the cart
+    def post(self):
+        #update
+        user = users.get_current_user()
+        if not user:
+            user = self.request.cookies.get('key')
+        else:
+            user = user.email()
+
+        id = self.request.get('id')
+        num = int(self.request.get('num'))
+
+        entry_query = cart_entry.query(ancestor = user_key(user))
+        entries = entry_query.fetch()
+
+        for entry in entries:
+            if entry.wine_id == id:
+                entry = entry.key.get()
+                entry.wine_num = num
+                entry.put()
+                if num == 0:
+                    self.redirect('/delete')
+
+        self.redirect('/cart')
+
+#addtocart
+class addtocart(webapp2.RequestHandler):
+    def post(self):
+        id = self.request.get('id')
+        num = int(self.request.get('num'))
+        user = users.get_current_user()
+        if not user:
+            user = self.request.cookies.get('key')
+        else:
+            user = user.email()
+
+
+        #check if in the cart
+        entry_query = cart_entry.query(ancestor = user_key(user)).order(-cart_entry.date)
+        entry = entry_query.filter(cart_entry.wine_id == id).fetch(1)
+
+        if not entry:
+            new_entry = cart_entry(parent = user_key(user))
+            new_entry.wine_id = id
+            new_entry.wine_num = num
+            new_entry.put()
+        else:
+            entry = entry.pop(0).key.get()
+            entry.wine_num = entry.wine_num + num
+            entry.put()
+
+        self.redirect('/wine')
+
+#delete
+class delete(webapp2.RequestHandler):
+    def post(self):
+        #update
+        user = users.get_current_user()
+        if not user:
+            user = self.request.cookies.get('key')
+        else:
+            user = user.email()
+
+        id = self.request.get('id')
+
+        entry_query = cart_entry.query(ancestor = user_key(user))
+        entries = entry_query.fetch()
+
+        for entry in entries:
+            if entry.wine_id == id:
+                entry = entry.key.delete()
+
+        self.redirect('/cart')
+
+#check out
+class checkout(webapp2.RequestHandler):
+    def post(self):
+        user = users.get_current_user()
+        cart_entries = cart_entry.query(ancestor = user_key(user.email()))
+        for entry in cart_entries:
+            new_entry = history_entry(parent = user_key(user.email()))
+            new_entry.user_id = user.email()
+            new_entry.wine_id = entry.wine_id
+            new_entry.wine_num = entry.wine_num
+            new_entry.put()
+            entry.key.delete()
+
+        self.redirect('/cart')
+
 # [START app]
 app = webapp2.WSGIApplication([
     ('/', MainPage),
     ('/wine', Wine),
     ('/enterinfo', Enter),
-    ('/search', Search)
+    ('/search', Search),
+    ('/cart', Cart),
+    ('/addtocart', addtocart),
+    ('/delete', delete),
+    ('/checkout', checkout)
 ], debug=True)
 # [END app]
